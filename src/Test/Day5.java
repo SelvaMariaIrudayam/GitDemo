32afc2abd835123b9714e7522ca6b1cf40b7f1eb
SelvaMariaIrudayam <selvaxavier.rajam@gmail.com>
2022-11-22 13:01:03 -0600

    This is a commit from the eclipse teams

---
+++ src/Test/Day5.java
@@ -0,0 +1,10 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Day5 {
+@Test
+public void finalTest() {
+	System.out.println("we have completed all tests in regressin suite");
+}
+}
